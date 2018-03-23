ae505215a38848a4fa82d9360b36194bcdeb0556
Mattias Persson <mattias@happypixels.se>
2018-03-23 10:38:13 +0100

    Use ES5 syntax for backwards compatability

--- index.js
+++ index.js
@@ -1,159 +1,161 @@
 const LocationBar = require("location-bar");
 const locationBar = new LocationBar();
 
-module.exports = {
-    enable(onChangeCallback, pushState){
-        let self = this;
+function Url(){}
 
-        // When the URL changes, run provided callback.
-        if(onChangeCallback){
-            locationBar.onChange(function (queryString) {
-                onChangeCallback({
-                    queryParams: self.getParams(), 
-                    queryString: queryString
-                });
+Url.prototype.enable = function(onChangeCallback, pushState){
+    var self = this;
+
+    // When the URL changes, run provided callback.
+    if(onChangeCallback){
+        locationBar.onChange(function (queryString) {
+            onChangeCallback({
+                queryParams: self.getParams(), 
+                queryString: queryString
             });
-        }
-
-        // Push state defaults to true but can be disabled.
-        if(typeof pushState !== 'boolean'){
-            pushState = true;
-        }
-
-        // Start the URL listener.
-        locationBar.start({ pushState: pushState });
-    },
-
-    apply(params){
-        if(typeof params === 'object'){
-            params = this.objToQueryString(params);
-        }
-
-        if(params.substring(0, 1) !== '?' && params.length > 0){
-            params = '?' + params;
-        }
-
-        locationBar.update(params, {trigger: true});
-    },
-
-    set(param, value){
-        let params = this.getParams();
-
-        params[param] = value;    
-        
-        this.apply(params);
-    },
-
-    get(param, fallback){
-        if(typeof fallback === 'undefined' || !fallback){
-            fallback = null;
-        }
-
-        let params = this.getParams();
-
-        if(params[param]){
-            return params[param];
-        }
-
-        return fallback;
-    },
-
-    toggle(param, value){
-        let params = this.getParams();
-
-        if(params[param]){
-            delete params[param];
-        } else {
-            params[param] = value;
-        }
-
-        this.apply(params);
-    },
-
-    toggleValue(param, value){
-        let params = this.getParams();
-
-        if(params[param]){
-            let values = params[param].split(',');
-            let valuePosition = values.indexOf(value.toString());
-            
-            if(valuePosition > -1){
-                values.splice(valuePosition, 1);
-            } else {
-                values.push(value);
-            }
-            
-            if(!values.length){
-                delete params[param];
-            } else {
-                params[param] = values.join(',');
-            }
-        } else {
-            params[param] = value;
-        }
-
-        this.apply(params);
-    },
-
-    replace(values){
-        let params = this.getParams();
-
-        Object.keys(values).map(key => {
-            params[key] = values[key];
         });
+    }
 
-        this.apply(params);
-    },
+    // Push state defaults to true but can be disabled.
+    if(typeof pushState !== 'boolean'){
+        pushState = true;
+    }
 
-    remove(param){
-        let params = this.getParams();
+    // Start the URL listener.
+    locationBar.start({ pushState: pushState });
+}
 
-        if(params[param]){
-            delete params[param];
-        }
+Url.prototype.apply = function(params){
+    if(typeof params === 'object'){
+        params = this.objToQueryString(params);
+    }
 
-        this.apply(params);
-    },
+    if(params.substring(0, 1) !== '?' && params.length > 0){
+        params = '?' + params;
+    }
 
-    clear(){
-        this.apply('');
-    },
+    locationBar.update(params, {trigger: true});
+}
 
-    containsValue(param, value){
-        let params = this.getParams();
+Url.prototype.set = function(param, value){
+    var params = this.getParams();
 
-        if(!params[param]){
-            return false;
-        }
+    params[param] = value;    
+    
+    this.apply(params);
+}
 
-        return (params[param].split(',').indexOf(value.toString()) > -1);
-    },
+Url.prototype.get = function(param, fallback){
+    if(typeof fallback === 'undefined' || !fallback){
+        fallback = null;
+    }
 
-    getParams(){
-        let search;
+    var params = this.getParams();
 
-        if(location.hash){
-            search = location.hash.substring(2);
-        } else if (location.search){
-            search = location.search.substring(1);
+    if(params[param]){
+        return params[param];
+    }
+
+    return fallback;
+}
+
+Url.prototype.toggle = function(param, value){
+    var params = this.getParams();
+
+    if(params[param]){
+        delete params[param];
+    } else {
+        params[param] = value;
+    }
+
+    this.apply(params);
+}
+
+Url.prototype.toggleValue = function(param, value){
+    var params = this.getParams();
+
+    if(params[param]){
+        var values = params[param].split(',');
+        var valuePosition = values.indexOf(value.toString());
+        
+        if(valuePosition > -1){
+            values.splice(valuePosition, 1);
+        } else {
+            values.push(value);
         }
         
-        if(!search){
-            return {};
+        if(!values.length){
+            delete params[param];
+        } else {
+            params[param] = values.join(',');
         }
-
-        return JSON.parse('{"' + decodeURI(search).replace(/"/g, '\\"').replace(/&/g, '","').replace(/=/g,'":"') + '"}');
-    },
-
-    objToQueryString(obj) {
-        let str = [];
-
-        for (var p in obj){
-            if (obj.hasOwnProperty(p)) {
-                str.push(encodeURIComponent(p) + "=" + obj[p]);
-            }
-        }
-
-        return str.join("&");
+    } else {
+        params[param] = value;
     }
+
+    this.apply(params);
 }
+
+Url.prototype.replace = function(values){
+    var params = this.getParams();
+
+    Object.keys(values).map(function(key){
+        params[key] = values[key];
+    });
+
+    this.apply(params);
+}
+
+Url.prototype.remove = function(param){
+    var params = this.getParams();
+
+    if(params[param]){
+        delete params[param];
+    }
+
+    this.apply(params);
+}
+
+Url.prototype.clear = function(){
+    this.apply('');
+}
+
+Url.prototype.containsValue = function(param, value){
+    var params = this.getParams();
+
+    if(!params[param]){
+        return false;
+    }
+
+    return (params[param].split(',').indexOf(value.toString()) > -1);
+}
+
+Url.prototype.objToQueryString = function(obj) {
+    var str = [];
+
+    for (var p in obj){
+        if (obj.hasOwnProperty(p)) {
+            str.push(encodeURIComponent(p) + "=" + obj[p]);
+        }
+    }
+
+    return str.join("&");
+}
+
+Url.prototype.getParams = function(){
+    var search;
+
+    if(location.hash){
+        search = location.hash.substring(2);
+    } else if (location.search){
+        search = location.search.substring(1);
+    }
+    
+    if(!search){
+        return {};
+    }
+
+    return JSON.parse('{"' + decodeURI(search).replace(/"/g, '\\"').replace(/&/g, '","').replace(/=/g,'":"') + '"}');
+}
+
+export default new Url();
